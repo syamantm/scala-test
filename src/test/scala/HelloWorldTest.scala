4f4d3a8e0c9e8879477bbf553949c20c58a31fa3
Syamantak Mukhopadhyay <mukherjee.syamantak@gmail.com>
2015-05-17 16:00:27 +0100

    First test

---
+++ src/test/scala/HelloWorldTest.scala
@@ -0,0 +1,13 @@
+import org.testng.Assert.assertEquals
+import org.testng.annotations.Test
+
+/**
+ * @author syamantak
+ */
+class HelloWorldTest {
+
+  @Test
+  def testSomething(): Unit = {
+    assertEquals(1, 1)
+  }
+}
